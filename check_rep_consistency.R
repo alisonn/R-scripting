0b9c3af416fe6388ae182bea790ab17f2f8562e9
Alison Nguyen <alisonn@Alisons-MBP.lmi.net>
2017-01-31 10:36:36 -0800

    added comments and fixed output column ordering

--- check_rep_consistency.R
+++ check_rep_consistency.R
@@ -1,14 +1,15 @@
 # Checks consistency between the biological replicates (same genotype and treatment)
-# for p-values and also frequency of summer or winter alleles
+# for p-values, frequency of summer or winter alleles, readDepth consistency
+# Output: 4 plots in pdf format and a .tsv with the merged replicates to avoid multiple file-handling downstream
 
-# Input from commandline
+# Input: rep1 and rep2 files - these are outputs from alisonn@github: python-scripting/find_parent_contribution.py
 args = commandArgs(TRUE)
 rep1_file = args[1]
 rep2_file = args[2]
 out_pdf = args[3]
 out_file = args[4]
 
-# Read in the two replicate files
+# Read in replicate files
 rep1 <- read.table(rep1_file, stringsAsFactors = FALSE, header = TRUE)
 rep2 <- read.table(rep2_file, stringsAsFactors = FALSE, header = TRUE)
 
@@ -26,6 +27,7 @@ reps_merged <- merge(x = rep1, y = rep2, by = "SNP_pos", all = TRUE)
 # Filter by read depth
 rd_cutoff = 100
 reps_merged_filtered <- subset(reps_merged, reps_merged$readDepth.x >= rd_cutoff & reps_merged$readDepth.y >= rd_cutoff)
+
 pdf(out_pdf, width = 10, height = 10)
 par(mfrow = c(2,2))
 
@@ -37,13 +39,15 @@ plot(log(reps_merged_filtered$p.value.x, base = 10) ~ log(reps_merged_filtered$p
 
 # Plot summer allele frequency of the replicates against one another to check for consistency
 # Are the allele frequencies at each SNP position consistent within the biological replicatesl
-
 plot(reps_merged_filtered$f.summer.x ~ reps_merged_filtered$f.summer.y, main = "Summer Allele Freq Replicate Consistency", ylab = "Replicate 1 Summer Allele Freq", xlab = "Replicate 2 Summer Allele Freq", cex = 0.6) 
+
 # plot readdepth at each snp site
 plot(reps_merged_filtered$readDepth.x ~ reps_merged_filtered$readDepth.y, ylim = c(100, 1000), xlim = c(100,1000), xlab = "Replicate 2 Read Depth", ylab = "Replicate 1 Read Depth", cex = 0.6, main = "Read Depth Consistency")
 dev.off()
 
-out_dataframe = reps_merged[, c(1, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17)]
+# Simplified dataframe for further downstream processing
+out_dataframe = reps_merged[, c(1, 4, 12, 5, 13, 6, 14, 7, 15, 8, 16, 9, 17)]
+
 # write the data.frame
 #x is rep1, y is rep2, keep all sites (no filters)
 write.table(out_dataframe, file = out_file, quote = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE) 
